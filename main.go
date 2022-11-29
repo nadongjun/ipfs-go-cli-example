5c44619c65678b7c24e9378566605efed7683c03
Dongjun Na <kmu5544616@gmail.com>
2022-11-29 13:59:41 +0900

    Added download

--- main.go
+++ main.go
@@ -9,11 +9,13 @@ import (
 )
 
 func main() {
-	var dataset = [5]string{"128kb", "64mb", "128mb", "256mb", "512mb"} //, "1024kb", "2048kb"}
+	//var dataset = [5]string{"128kb", "64mb", "128mb", "256mb", "512mb"}                                                                                                                                                                                                                     //, "1024kb", "2048kb"}
+	var query_dataset = [5]string{"QmZFbKpxLdSBMTjvoqwknYn2bmbcRVynZX2CyfRPhqjHtL", "QmUpWVqNF2fh639AVe2s3b58SuuBkW39qtMgLDDU43Rt6s", "QmZFbKpxLdSBMTjvoqwknYn2bmbcRVynZX2CyfRPhqjHtL", "QmYKGy9y4kAteLU1sLUCnRMemfi4gsH2ea1Vdn9Y8Wr9UN", "QmVjuwPYXCfcaRWCCrXViSRDpvFEq8UqXqfqZGJDAhZRnk"} //, "1024kb", "2048kb"}
+
 	sh := shell.NewShell("54.176.172.176:5001")
 	time.Sleep(5 * time.Second)
 
-	for _, s := range dataset {
+	/*for _, s := range dataset {
 
 		fmt.Println(s)
 		startTime := time.Now()
@@ -25,6 +27,23 @@ func main() {
 			os.Exit(1)
 		}
 		fmt.Println("added ", cid)
+		elapsedTime := time.Since(startTime)
+
+		fmt.Printf("실행시간: %s\n", elapsedTime)
+		time.Sleep(5 * time.Second)
+	}*/
+
+	for _, s := range query_dataset {
+
+		fmt.Println(s)
+		startTime := time.Now()
+
+		err := sh.Get(s, s+".txt")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %s", err)
+			os.Exit(1)
+		}
+
 		elapsedTime := time.Since(startTime)
 
 		fmt.Printf("실행시간: %s\n", elapsedTime)
